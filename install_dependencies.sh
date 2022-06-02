dd2231b88c001c8433fcaf6476e2d00c69ff77e0
Sachin <sachin.guruswamy@gmail.com>
2022-06-02 11:24:15 -0700

    Trimmed the branch to only the depthai_bridge sub directory

--- install_dependencies.sh
+++
@@ -1,8 +0,0 @@
-set -e
-sudo wget -qO- https://raw.githubusercontent.com/luxonis/depthai-docs-website/master/source/_static/install_dependencies.sh | bash 
-cd /tmp
-git clone --recursive https://github.com/luxonis/depthai-core.git --branch main
-cmake -Hdepthai-core -Bdepthai-core/build -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=/usr/local
-cmake --build depthai-core/build --target install
-cd /tmp
-rm -r depthai-core
