66e7637f6b6d679eddfe23f926ca7bc9a0beb08a
Serafadam <serafadam@gmail.com>
2023-04-25 12:43:43 +0200

    Rebase from 'upstream'

--- depthai_bridge/include/depthai_bridge/ImuConverter.hpp
+++ depthai_bridge/include/depthai_bridge/ImuConverter.hpp
@@ -1,34 +1,23 @@
 #pragma once
 
-#include <depthai/depthai.hpp>
-#include <depthai_bridge/depthaiUtility.hpp>
+#include <deque>
 #include <iostream>
-#include <opencv2/opencv.hpp>
+#include <memory>
 #include <sstream>
+#include <string>
 #include <unordered_map>
 
-#ifdef IS_ROS2
-    #include "rclcpp/rclcpp.hpp"
-    #include "sensor_msgs/msg/imu.hpp"
-#else
-    #include <ros/ros.h>
-
-    #include <boost/make_shared.hpp>
-
-    #include "sensor_msgs/Imu.h"
-#endif
+#include "depthai-shared/datatype/RawIMUData.hpp"
+#include "depthai/pipeline/datatype/IMUData.hpp"
+#include "rclcpp/time.hpp"
+#include "sensor_msgs/msg/imu.hpp"
 
 namespace dai {
 
 namespace ros {
 
-#ifdef IS_ROS2
 namespace ImuMsgs = sensor_msgs::msg;
 using ImuPtr = ImuMsgs::Imu::SharedPtr;
-#else
-namespace ImuMsgs = sensor_msgs;
-using ImuPtr = ImuMsgs::Imu::Ptr;
-#endif
 
 enum class ImuSyncMethod { COPY, LINEAR_INTERPOLATE_GYRO, LINEAR_INTERPOLATE_ACCEL };
 
@@ -38,7 +27,7 @@ class ImuConverter {
                  ImuSyncMethod syncMode = ImuSyncMethod::LINEAR_INTERPOLATE_ACCEL,
                  double linear_accel_cov = 0.0,
                  double angular_velocity_cov = 0.0);
-
+    ~ImuConverter();
     void toRosMsg(std::shared_ptr<dai::IMUData> inData, std::deque<ImuMsgs::Imu>& outImuMsg);
 
    private:
@@ -50,11 +39,7 @@ class ImuConverter {
     const std::string _frameName = "";
     ImuSyncMethod _syncMode;
     std::chrono::time_point<std::chrono::steady_clock> _steadyBaseTime;
-#ifdef IS_ROS2
     rclcpp::Time _rosBaseTime;
-#else
-    ::ros::Time _rosBaseTime;
-#endif
 };
 
 }  // namespace ros
