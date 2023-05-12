dd7b4815ec82e986793a2ed43df267e52208b07e
Serafadam <serafadam@gmail.com>
2023-05-12 16:41:44 +0200

    Revert "Trimmed the branch to only the depthai-ros sub directory"
    This reverts commit 5740c1299efb587ed5de1254e034f2d713427f7d.

---
+++ depthai_bridge/include/depthai_bridge/DisparityConverter.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <deque>
+#include <memory>
+#include <string>
+
+#include "depthai/pipeline/datatype/ImgFrame.hpp"
+#include "rclcpp/time.hpp"
+#include "sensor_msgs/image_encodings.hpp"
+#include "stereo_msgs/msg/disparity_image.hpp"
+
+namespace dai {
+
+namespace ros {
+
+namespace DisparityMsgs = stereo_msgs::msg;
+namespace ImageMsgs = sensor_msgs::msg;
+using ImagePtr = ImageMsgs::Image::SharedPtr;
+using DisparityImagePtr = DisparityMsgs::DisparityImage::SharedPtr;
+
+using TimePoint = std::chrono::time_point<std::chrono::steady_clock, std::chrono::steady_clock::duration>;
+
+class DisparityConverter {
+   public:
+    DisparityConverter(
+        const std::string frameName, float focalLength, float baseline = 7.5, float minDepth = 80, float maxDepth = 1100, bool getBaseDeviceTimestamp = false);
+    ~DisparityConverter();
+    void toRosMsg(std::shared_ptr<dai::ImgFrame> inData, std::deque<DisparityMsgs::DisparityImage>& outImageMsg);
+    DisparityImagePtr toRosMsgPtr(std::shared_ptr<dai::ImgFrame> inData);
+
+    // void toDaiMsg(const DisparityMsgs::DisparityImage& inMsg, dai::ImgFrame& outData);
+
+   private:
+    const std::string _frameName = "";
+    const float _focalLength = 882.2, _baseline = 7.5, _minDepth = 80, _maxDepth;
+    std::chrono::time_point<std::chrono::steady_clock> _steadyBaseTime;
+    rclcpp::Time _rosBaseTime;
+    bool _getBaseDeviceTimestamp;
+};
+
+}  // namespace ros
+
+namespace rosBridge = ros;
+
+}  // namespace dai
