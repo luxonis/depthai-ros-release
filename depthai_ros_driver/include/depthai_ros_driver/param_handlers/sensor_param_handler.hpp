31061b7e64c2037baae20c6669fe3b69011cd506
Serafadam <serafadam@gmail.com>
2025-03-17 19:08:13 +0100

    Revert "Trimmed the branch to only the depthai_bridge sub directory"
    This reverts commit 98ffd40086409d15f72ae4066eeec5c57dc4be9f.

---
+++ depthai_ros_driver/include/depthai_ros_driver/param_handlers/sensor_param_handler.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "depthai-shared/common/CameraFeatures.hpp"
+#include "depthai/pipeline/datatype/CameraControl.hpp"
+#include "depthai_ros_driver/dai_nodes/sensors/sensor_helpers.hpp"
+#include "depthai_ros_driver/param_handlers/base_param_handler.hpp"
+#include "depthai_ros_driver/parametersConfig.h"
+
+namespace dai {
+namespace node {
+class MonoCamera;
+class ColorCamera;
+class Camera;
+}  // namespace node
+}  // namespace dai
+
+namespace ros {
+class NodeHandle;
+}  // namespace ros
+
+namespace depthai_ros_driver {
+namespace param_handlers {
+class SensorParamHandler : public BaseParamHandler {
+   public:
+    explicit SensorParamHandler(ros::NodeHandle node, const std::string& name, dai::CameraBoardSocket socket);
+    ~SensorParamHandler();
+    void declareCommonParams(dai::CameraBoardSocket socket);
+    void declareParams(std::shared_ptr<dai::node::MonoCamera> monoCam, dai_nodes::sensor_helpers::ImageSensor sensor, bool publish);
+    void declareParams(std::shared_ptr<dai::node::ColorCamera> colorCam, dai_nodes::sensor_helpers::ImageSensor sensor, bool publish);
+    void declareParams(std::shared_ptr<dai::node::Camera> cam, dai::CameraFeatures, bool publish);
+    dai::CameraControl setRuntimeParams(parametersConfig& config) override;
+
+   private:
+    dai::CameraBoardSocket socketID;
+};
+}  // namespace param_handlers
+}  // namespace depthai_ros_driver
