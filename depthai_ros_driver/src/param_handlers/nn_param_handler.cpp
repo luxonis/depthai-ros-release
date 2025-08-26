5fcfc392d171e4d752e7e70e4a4e319b4bf24dc5
Serafadam <serafadam@gmail.com>
2025-08-26 21:36:05 +0200

    Revert "Trimmed the branch to only the depthai_descriptions sub directory"
    This reverts commit 181ec94d43b43498658005e7abaf13334552316a.

---
+++ depthai_ros_driver/src/param_handlers/nn_param_handler.cpp
@@ -0,0 +1,43 @@
+#include "depthai_ros_driver/param_handlers/nn_param_handler.hpp"
+
+#include "depthai/common/CameraBoardSocket.hpp"
+#include "depthai/pipeline/node/DetectionNetwork.hpp"
+#include "depthai/pipeline/node/NeuralNetwork.hpp"
+#include "depthai/pipeline/node/SpatialDetectionNetwork.hpp"
+#include "depthai_ros_driver/utils.hpp"
+#include "nlohmann/json.hpp"
+#include "rclcpp/node.hpp"
+
+namespace depthai_ros_driver {
+namespace param_handlers {
+
+NNParamHandler::NNParamHandler(
+    std::shared_ptr<rclcpp::Node> node, const std::string& name, const std::string& deviceName, bool rsCompat, const dai::CameraBoardSocket& socket)
+    : BaseParamHandler(node, name, deviceName, rsCompat) {
+    nnFamilyMap = {
+        {"segmentation", nn::NNFamily::Segmentation},
+        {"detection", nn::NNFamily::Detection},
+    };
+    declareAndLogParam<int>("i_board_socket_id", static_cast<int>(socket));
+    declareAndLogParam<int>("i_max_q_size", 8);
+}
+NNParamHandler::~NNParamHandler() = default;
+nn::NNFamily NNParamHandler::getNNFamily() {
+    std::string nnFamily = declareAndLogParam<std::string>("i_nn_family", "detection");
+    declareAndLogParam<std::string>("i_nn_model", "yolov6-nano");
+    return utils::getValFromMap(nnFamily, nnFamilyMap);
+}
+
+void NNParamHandler::setNNParams(std::shared_ptr<dai::node::NeuralNetwork> /*nn*/) {}
+
+void NNParamHandler::setNNParams(std::shared_ptr<dai::node::DetectionNetwork> nn) {
+    nn->setConfidenceThreshold(declareAndLogParam<float>("i_nn_confidence_threshold", 0.5));
+}
+
+void NNParamHandler::setNNParams(std::shared_ptr<dai::node::SpatialDetectionNetwork> nn) {
+    nn->setConfidenceThreshold(declareAndLogParam<float>("i_nn_confidence_threshold", 0.5));
+    setSpatialParams(nn);
+}
+
+}  // namespace param_handlers
+}  // namespace depthai_ros_driver
