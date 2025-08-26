9a886152deb614b94316864c6de4d404d0bc264d
Serafadam <serafadam@gmail.com>
2025-08-26 21:36:08 +0200

    Rebase from 'upstream'

--- depthai_ros_driver/include/depthai_ros_driver/dai_nodes/sensors/tof.hpp
+++ depthai_ros_driver/include/depthai_ros_driver/dai_nodes/sensors/tof.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <depthai/common/CameraBoardSocket.hpp>
+
 #include "depthai_ros_driver/dai_nodes/base_node.hpp"
 #include "depthai_ros_driver/dai_nodes/sensors/sensor_helpers.hpp"
 
@@ -11,7 +13,6 @@ class ADatatype;
 namespace node {
 class ToF;
 class ImageAlign;
-class XLinkIn;
 }  // namespace node
 }  // namespace dai
 
@@ -40,19 +41,24 @@ class ToF : public BaseNode {
     ~ToF();
     void updateParams(const std::vector<rclcpp::Parameter>& params) override;
     void setupQueues(std::shared_ptr<dai::Device> device) override;
-    void link(dai::Node::Input in, int linkType = 0) override;
+    void link(dai::Node::Input& in, int linkType = 0) override;
     void setNames() override;
     void setInOut(std::shared_ptr<dai::Pipeline> pipeline) override;
+    dai::Node::Input& getInput(int linkType = 0) override;
     std::vector<std::shared_ptr<sensor_helpers::ImagePublisher>> getPublishers() override;
     void closeQueues() override;
     std::shared_ptr<dai::node::ToF> getUnderlyingNode();
+    dai::CameraBoardSocket getSocketID();
+    bool isAligned();
 
    private:
     std::shared_ptr<sensor_helpers::ImagePublisher> tofPub;
     std::shared_ptr<dai::node::ToF> tofNode;
+    std::shared_ptr<dai::node::ImageAlign> alignNode;
     std::unique_ptr<param_handlers::ToFParamHandler> ph;
     dai::CameraBoardSocket boardSocket;
     std::string tofQName;
+    bool aligned;
 };
 
 }  // namespace dai_nodes
