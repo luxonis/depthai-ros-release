9bcda2698bdd3d473f89faeb24cb7502f9c12f3e
Serafadam <serafadam@gmail.com>
2025-09-04 16:35:29 +0200

    Revert "Trimmed the branch to only the depthai_ros_msgs sub directory"
    This reverts commit 3c8503a9ed3a72e46b37592054fcd07799c664b5.

---
+++ depthai_ros_driver/src/param_handlers/rgbd_param_handler.cpp
@@ -0,0 +1,26 @@
+#include "depthai_ros_driver/param_handlers/rgbd_param_handler.hpp"
+
+#include <depthai/common/CameraBoardSocket.hpp>
+
+#include "depthai/pipeline/node/host/RGBD.hpp"
+#include "depthai_ros_driver/utils.hpp"
+#include "rclcpp/node.hpp"
+
+namespace depthai_ros_driver {
+namespace param_handlers {
+RGBDParamHandler::RGBDParamHandler(std::shared_ptr<rclcpp::Node> node, const std::string& name, const std::string& deviceName, bool rsCompat)
+    : BaseParamHandler(node, name, deviceName, rsCompat) {}
+RGBDParamHandler::~RGBDParamHandler() = default;
+void RGBDParamHandler::declareParams(std::shared_ptr<dai::node::RGBD> rgbd, dai::CameraBoardSocket socket) {
+    declareAndLogParam<bool>(ParamNames::PUBLISH_TOPIC, true);
+    declareAndLogParam<int>(ParamNames::BOARD_SOCKET_ID, static_cast<int>(socket));
+    declareAndLogParam<int>(ParamNames::MAX_Q_SIZE, 2);
+    declareAndLogParam<bool>(ParamNames::GET_BASE_DEVICE_TIMESTAMP, false);
+    declareAndLogParam<bool>(ParamNames::UPDATE_ROS_BASE_TIME_ON_ROS_MSG, false);
+    declareAndLogParam<int>("i_num_threads", 1);
+    declareAndLogParam<bool>("i_run_sync_on_host", true);
+    declareAndLogParam<bool>("i_run_align_on_host", true);
+}
+
+}  // namespace param_handlers
+}  // namespace depthai_ros_driver
