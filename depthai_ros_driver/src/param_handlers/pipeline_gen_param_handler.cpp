33f44ad0fc26b65c336a0216160276abb7718981
Matevz Morato <matevz.morato@gmail.com>
2026-05-15 17:10:25 +0200

    Revert "Trimmed the branch to only the depthai_bridge sub directory"
    This reverts commit 326187fdf6304560d8aaa659e1a4a5075a5aada5.

---
+++ depthai_ros_driver/src/param_handlers/pipeline_gen_param_handler.cpp
@@ -0,0 +1,22 @@
+
+#include "depthai_ros_driver/param_handlers/pipeline_gen_param_handler.hpp"
+
+#include "rclcpp/node.hpp"
+
+namespace depthai_ros_driver {
+namespace param_handlers {
+PipelineGenParamHandler::PipelineGenParamHandler(std::shared_ptr<rclcpp::Node> node, const std::string& name, const std::string& deviceName, bool rsCompat)
+    : BaseParamHandler(node, name, deviceName, rsCompat) {}
+PipelineGenParamHandler::~PipelineGenParamHandler() = default;
+
+void PipelineGenParamHandler::declareParams() {
+    declareAndLogParam<std::string>("i_pipeline_type", "RGBD");
+    declareAndLogParam<std::string>("i_nn_type", "none");
+    declareAndLogParam<bool>("i_enable_imu", true);
+    declareAndLogParam<bool>("i_enable_diagnostics", false);
+    declareAndLogParam<bool>("i_enable_rgbd", false);
+    declareAndLogParam<bool>("i_enable_vio", false);
+    // declareAndLogParam<bool>("i_enable_slam", false);
+}
+}  // namespace param_handlers
+}  // namespace depthai_ros_driver
